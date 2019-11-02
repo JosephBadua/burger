ebeade1db835e09b629f5794a415c5777b64e35e
JosephBadua <josephbadua1006@gmail.com>
2019-11-02 12:32:58 -0700

    made the queries for the orm

--- models/burger.js
+++ models/burger.js
@@ -1,2 +1,27 @@
 const orm = require("../config/orm.js");
 console.log(orm);
+
+const burger = {
+    findAll: function(callback){
+        orm.findAll("burgers", function(res){
+            callback(res);
+        })
+    },
+    create: function(cols, vals, callback){
+        orm.create("burgers", cols, vals, function(res){
+            callback(res);
+        })
+    },
+    update: function(objColVals, condition, callback){
+        orm.update("burgers", objColVals, condition, function(res){
+            callback(res);
+        })
+    },
+    delete: function(condtion, callback){
+        orm.delete("burgers", condtion, function(res){
+            callback(res);
+        });
+    }
+};
+
+module.exports = burger;
