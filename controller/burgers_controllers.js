ab43e4d66bc9b9bd586c86643d58d8857f4ea14e
JosephBadua <josephbadua1006@gmail.com>
2019-11-04 15:28:27 -0800

    was able to make my databse display through handlebars

--- controller/burgers_controllers.js
+++ controller/burgers_controllers.js
@@ -3,9 +3,9 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 router.get("/", function(req, res){
-    burger.all(function(burger_data){
-        console.log(burger_data);
-        res.render("index");
+    burger.all(function(burgers){
+        console.log(burgers);
+        res.render("index", {burgers});
     })
 });
 
