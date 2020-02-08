614477dc3a732c4b50b820724e99ab5f677ce9f9
Joseph Badua <52088997+JosephBadua@users.noreply.github.com>
2020-02-08 01:32:34 -0800

    added images and extra styling

--- public/assets/javascript/burgerPage.js
+++ public/assets/javascript/burgerPage.js
@@ -1,6 +1,36 @@
-var time = new Date();
-console.log(time)
-
+let time = new Date();
+let day = time.getDay();
+console.log(day)
 //Daily Special
 //Eat Sound
 //Add date w/ eaten
+
+const specialOne = document.querySelector('.specialDay');
+specialOne.textContent = "balls"
+console.log(specialOne)
+
+switch(day){
+    case 0:
+        specialOne.textContent = "Super Size Sunday!"
+        break;
+    case 1:
+        specialOne.textContent = "Extra Mayo Monday!"
+        break;
+    case 2:
+        specialOne.textContent = "Double Patty Tuesday!"
+        break;
+    case 3:
+        specialOne.textContent = "Which Sandwich Wednesday!"
+        break;
+    case 4:
+        specialOne.textContent = "Tons of Tomatoes Thursday!"
+        break;
+    case 5:
+        specialOne.textContent = "Fried Veggies Friday"
+        break;
+    case 6:
+        specialOne.textContent = "Super Saturday Special"
+        break;
+    default: 
+        specialOne.textContent = "Buy 1 Get One Free!"
+}
